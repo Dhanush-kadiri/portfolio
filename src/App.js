fe0eba0b4f4bb3c80bd7190c5e609e3971d10c86
Dhanush-kadiri <150281871+Dhanush-kadiri@users.noreply.github.com>
2024-07-19 13:59:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-import { useEffect, useState } from "react";
-import Nav from "./project/templates/Navbar";
-import Footer from "./project/templates/Footer";
-import Home from "./project/templates/Home";
-import Type from "./project/templates/Type";
-import About from "./project/templates/AboutMe";
-import Projects from "./project/templates/Projects";
-import Contact from "./project/templates/Contact";
-import MoveToTop from "./project/templates/MoveToTop";
-import CircleLoader from "react-spinners/CircleLoader";
-import "./App.css";
-import Skills from "./project/templates/Skills";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { useEffect, useState } from 'react';
+import Nav from './project/templates/Navbar';
+import Footer from './project/templates/Footer';
+import Home from './project/templates/Home';
+import Type from './project/templates/Type';
+import About from './project/templates/AboutMe';
+import Projects from './project/templates/Projects';
+import Contact from './project/templates/Contact';
+import Skills from './project/templates/Skills';
+import MoveToTop from './project/templates/MoveToTop';
+import CircleLoader from 'react-spinners/CircleLoader';
+import './App.css';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -39,15 +39,19 @@ function App() {
         <BrowserRouter basename="/portfolio">
           <Nav />
           <TransitionGroup>
-            <CSSTransition classNames="fade" timeout={500}>
+            <CSSTransition
+              timeout={500}
+              classNames="fade"
+              key={window.location.pathname}
+            >
               <Routes>
-                <Route path="/" element={<Navigate to="/portfolio" replace />} />
-                <Route path="/portfolio" element={<Home />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/Type" element={<Type />} />
                 <Route path="/AboutMe" element={<About />} />
                 <Route path="/Skills" element={<Skills />} />
                 <Route path="/Contact" element={<Contact />} />
                 <Route path="/Projects" element={<Projects />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
               </Routes>
             </CSSTransition>
           </TransitionGroup>
