3da092e50eb3dd0cbdf39454db10393114cfc25c
Dhanush-kadiri <150281871+Dhanush-kadiri@users.noreply.github.com>
2024-07-19 15:09:13 +0530

    Update Contact.jsx

--- src/project/templates/Contact.jsx
+++ src/project/templates/Contact.jsx
@@ -39,16 +39,26 @@ const Contact = () => {
       <h2>Get In Touch</h2>
       <div className="contact-options"  >
         <Tilt>
-        <button className="contact-option" onClick={handleWhatsAppClick} style={{height:100,width:250, marginLeft:100}}>
+{/*         <button className="contact-option" onClick={handleWhatsAppClick} style={{height:100,width:250, marginLeft:100}}>
           <FaWhatsapp className="icon" style={{height:50,width:50}} />
           Contact via WhatsApp
+        </button> */}
+         <button className="contact-option" onClick={handleWhatsAppClick} style={{height:'auto',width:'auto', marginLeft:'auto'}}>
+          <FaWhatsapp className="icon" style={{height:'auto',width:'auto}} />
+          Contact via WhatsApp
         </button></Tilt>
-        <div className="or" style={{fontSize:25, alignItems:'center', marginTop:25,marginLeft:200}}> <b>OR</b></div>
+{/*         <div className="or" style={{fontSize:25, alignItems:'center', marginTop:25,marginLeft:200}}> <b>OR</b></div> */}
+         <div className="or" style={{fontSize:'auto', alignItems:'center', marginTop:'auto',marginLeft:'auto'}}> <b>OR</b></div>
         <Tilt>
-        <button className="contact-option" onClick={handleEmailClick} style={{height:100,width:250, marginLeft:100}}>
+{/*         <button className="contact-option" onClick={handleEmailClick} style={{height:100,width:250, marginLeft:100}}>
           <FaEnvelope className="icon" style={{height:50,width:50}} />
           Contact via Email
-        </button></Tilt>
+        </button> */}
+          <button className="contact-option" onClick={handleEmailClick} style={{height:'auto',width:'auto', marginLeft:'auto'}}>
+          <FaEnvelope className="icon" style={{height:'auto',width:'auto'}} />
+          Contact via Email
+        </button>
+        </Tilt>
       </div>
 
       {showForm && (
