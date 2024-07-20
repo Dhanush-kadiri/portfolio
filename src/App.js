11b5f3b9f1d99f25beb7df9fc163c9886767e398
Dhanush-kadiri <kadiridhanush143@gmail.com>
2024-07-20 09:44:28 +0530

    Fix default route to load Home page

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
       ) : (
         <BrowserRouter>
           <Nav />
+          <Home />
           <TransitionGroup>
             <CSSTransition classNames="fade" timeout={500}>
             <Routes>
