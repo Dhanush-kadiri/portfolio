a47dd3cd9625705ff27e80421349f2f4f59b7f73
Dhanush-kadiri <150281871+Dhanush-kadiri@users.noreply.github.com>
2024-07-20 10:52:37 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,68 +1,4 @@
-// import { Route, Routes, BrowserRouter } from "react-router-dom";
-// import { TransitionGroup, CSSTransition } from "react-transition-group";
-// import { useEffect, useState } from "react";
-// import Nav from "./project/templates/Navbar";
-// import Footer from "./project/templates/Footer";
-// import Home from "./project/templates/Home";
-// import Type from "./project/templates/Type";
-// import About from "./project/templates/AboutMe";
-// import Projects from "./project/templates/Projects";
-// import Contact from "./project/templates/Contact";
-// import MoveToTop from "./project/templates/MoveToTop";
-// import CircleLoader from "react-spinners/CircleLoader";
-// import "./App.css";
-// import Skills from "./project/templates/Skills";
-
-// function App() {
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     setLoading(true);
-//     setTimeout(() => {
-//       setLoading(false);
-//     }, 1900);
-//   }, []);
-
-//   return (
-//     <div>
-//       {loading ? (
-//         <div className="loader">
-//           <CircleLoader
-//             color={"#011c38"}
-//             loading={true}
-//             size={100}
-//             aria-label="Loading Spinner"
-//             data-testid="loader"
-//           />
-//         </div>
-//       ) : (
-//         <BrowserRouter>
-//           <Nav />
-//           <Home/>
-//           <TransitionGroup>
-//             <CSSTransition classNames="fade" timeout={500}>
-//             <Routes>
-//       <Route path='/' element={<Home/>} />
-//       <Route path='/Type' element={<Type/>}  />
-//       <Route path='/AboutMe'  element={<About/>}/>
-//       <Route  path='/Skills' element={< Skills />}/>
-//        <Route  path='/Contact' element={<Contact/>} />
-//       <Route path='/Projects' element={<Projects/>} />
-//       </Routes>
-//             </CSSTransition>
-//           </TransitionGroup>
-  
-//           <Footer />
-//           <MoveToTop />
-//         </BrowserRouter>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { useEffect, useState } from "react";
 import Nav from "./project/templates/Navbar";
@@ -88,7 +24,7 @@ function App() {
   }, []);
 
   return (
-    <Router basename="/portfolio">
+    <div>
       {loading ? (
         <div className="loader">
           <CircleLoader
@@ -100,27 +36,28 @@ function App() {
           />
         </div>
       ) : (
-        <div>
+        <BrowserRouter>
           <Nav />
-         
           <TransitionGroup>
             <CSSTransition classNames="fade" timeout={500}>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Type" element={<Type />} />
-                <Route path="/AboutMe" element={<About />} />
-                <Route path="/Skills" element={<Skills />} />
-                <Route path="/Contact" element={<Contact />} />
-                <Route path="/Projects" element={<Projects />} />
-              </Routes>
+            <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/Type' element={<Type/>}  />
+      <Route path='/AboutMe'  element={<About/>}/>
+      <Route  path='/Skills' element={< Skills />}/>
+       <Route  path='/Contact' element={<Contact/>} />
+      <Route path='/Projects' element={<Projects/>} />
+      </Routes>
             </CSSTransition>
           </TransitionGroup>
+  
           <Footer />
           <MoveToTop />
-        </div>
+        </BrowserRouter>
       )}
-    </Router>
+    </div>
   );
 }
 
 export default App;
+
